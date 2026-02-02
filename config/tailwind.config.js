23a4afb70d6cf779553149a7996a24003832d837
Tonny Donghwi Kim <tonny.kim@timberay.com>
2026-02-02 21:21:20 +0900

    feat: configure TailwindCSS dark mode

---
+++ config/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    './app/views/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/assets/stylesheets/**/*.css',
+  ],
+  darkMode: 'class', // Enable class-based dark mode
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
