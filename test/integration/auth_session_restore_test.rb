c87ca747c8bb1eb7501f3f28bbf4b7f5c09eb5eb
Tonny Donghwi Kim <tonny.kim@timberay.com>
2026-04-23 16:14:06 +0900

    feat(auth): ApplicationController#current_user + session restore

---
+++ test/integration/auth_session_restore_test.rb
@@ -0,0 +1,24 @@
+require "test_helper"
+
+class AuthSessionRestoreTest < ActionDispatch::IntegrationTest
+  test "current_user is nil when not signed in" do
+    get "/"
+    assert_response :ok
+    assert_nil controller.current_user
+  end
+
+  test "session[:user_id] restores current_user" do
+    post "/testing/sign_in", params: { user_id: users(:tonny).id }
+    get "/"
+    assert_response :ok
+    assert_equal users(:tonny), controller.current_user
+  end
+
+  test "stale session[:user_id] (user deleted) silently resets" do
+    deleted_id = 999_999
+    post "/testing/sign_in", params: { user_id: deleted_id }
+    get "/"
+    assert_response :ok
+    assert_nil controller.current_user
+  end
+end
